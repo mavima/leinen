2daa557ec0b2f6c682a23c3785c5c6018401b9d1
mavima <maria.vihla@gmail.com>
2023-05-25 12:04:28 +0200

    repository and interface files for item controller

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,27 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
-use App\Models\Item;
+use App\Repositories\ItemRepository;
 
 class ItemController extends Controller
 {
+    private ItemRepository $itemRepository;
+
+    public function __construct(ItemRepository $itemRepository)
+    {
+        $this->itemRepository = $itemRepository;
+    }
 
     public function index(Request $request) {
+
+
         $categories = Category::all();
-        if (count($request->all()) > 0 && $request->category_id == null) {
-        $search = $request->input('search');
-        $items = Item::query()
-            ->where('name', 'LIKE', "%{$search}%")
-            ->orWhere('description', 'LIKE', "%{$search}%")
-            ->get();
-        } else {
-            $items = Item::orderBy('id', 'desc')
-            ->with('category')
-            ->when($request->category_id, function ($query) use ($request) {
-                $query->where('category_id', $request->category_id);
-            })
-            ->get();
-        }
+        $items = $this->itemRepository->getItems($request);
         return view('index', ['items'=>$items, 'categories' => $categories]);
     }
 }
